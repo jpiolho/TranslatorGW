4585036a97ff7a371440f2de73dcd8a927068b9c
JPiolho <783467+jpiolho@users.noreply.github.com>
2023-06-28 21:07:08 +0200

    NC: Fixed Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -162,8 +162,8 @@ param (
     $UseScriptDirectory=$True,
 
     ## => User Config <= ## 
-    $ProjectPath = "GWReloaded.csproj",
-    $PackageName = "GWReloaded",
+    $ProjectPath = "TranslatorGW.csproj",
+    $PackageName = "TranslatorGW",
     $PublishOutputDir = "Publish/ToUpload",
 
     ## => User: Delta Config
@@ -180,7 +180,7 @@ param (
 
     $GameBananaItemId = 333681, # From mod page URL.
 
-    $NuGetPackageId = "GWReloaded",
+    $NuGetPackageId = "TranslatorGW",
     $NuGetFeedUrl = "http://packages.sewer56.moe:5000/v3/index.json",
     $NuGetAllowUnlisted = $False,
 
